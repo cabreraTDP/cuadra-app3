1e99bb1ed9b71224043b98222a848959a4957eb4
EleazarTDP <eleazarcabrera@techdatapeople.com>
2022-03-30 22:37:45 -0600

    preparing nominas

--- src/Components/Modules/Nominas/Nominas.js
+++ src/Components/Modules/Nominas/Nominas.js
@@ -1,8 +1,37 @@
 import TableDisplay from "../../TableDisplay"
 import { titles, data, options } from '../../../datafake';
+import { useEffect } from "react";
+import axios from "axios";
+import { useState } from "react";
 
 const Nominas = () => {
+    const [nominas, setNominas] = useState([]);
+    const [dataNominas, setDataNominas] = useState([{
+        "Nombre": "",
+        "RFC": "",
+        "CURP": "",
+        "NSS": ""
+    }]);
 
+    useEffect(async()=>{
+        const trabajadores = await axios.get('http://localhost:7799/nominas', {withCredentials: true});
+        setNominas(trabajadores.data.data);
+        setDataNominas(trabajadores.data.data.map((trabajador) => (
+            trabajador.datosPersonales ? {
+                "Nombre": trabajador.datosPersonales.nombre,
+                "RFC": trabajador.datosPersonales.rfc,
+                "CURP": trabajador.datosPersonales.curp,
+                "NSS": trabajador.datosPersonales.nss
+            } :
+            {
+                "Nombre": '',
+                "RFC": "",
+                "CURP": "",
+                "NSS": ""
+            }
+        )));
+
+    },[]);
     return (
         <div >
             <h1>Nominas</h1>

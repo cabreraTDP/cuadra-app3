39115b815128a0bbebff1fd3685293d28f47e6c1
GuillermoTDP <guillermomartinez@techdatapeople.com>
2022-05-22 02:21:58 -0500

    estructura analisis

--- src/Components/Modules/Contabilidad/Analisis.js
+++ src/Components/Modules/Contabilidad/Analisis.js
@@ -23,46 +23,126 @@ const Analisis = () => {
                 </div>
             </div>
             <div id='contenido'>
-                <div id='separador'>
-                    <div>
-                        <h5>Ingresos</h5>
-                    </div>
-                    <div>
-                        <div>
-                            pago de clientes $304,214.20
-                        </div>
-                        <div>
-                            Intereses Financieros $1,024.00
-                        </div>
+                <div >
+                    <table id='tabla'>
+                        <tr >
+                            <td rowSpan={2} id='tabla' style={{width:'80px'}}>
+                                Icono
+                            </td>
+                            <td id='tabla'>
+                                <h5>Ingresos</h5>
+                            </td>
 
-                    </div>
-                </div>
-                <div id='separador'>
-                    <div>
-                        <h5>Gastos</h5>
-                    </div>
-                    <div>
-                        <div>
-                            Sueldos $91,137.22
-                        </div>
-                        <div>
-                            Impuestos  $,037.14
-                        </div>
-                        <div>
-                            Operativos  $175,446.83
-                        </div>
+                        </tr>
 
-                    </div>
+                        <tr>
+                            <td id='tabla'>
+
+                            </td>
+
+
+                            <td id='tabla'>
+                                <h6>$305,238.20</h6>
+                            </td>
+
+                        </tr>
+                        <tr>
+                            <td id="tabla" >
+                                Pagos de Clientes
+                            </td>
+                            <td id='tabla' colSpan={2} >
+                                $304,214.20
+                            </td>
+
+                        </tr>
+                        <tr>
+                            <td id="tabla">
+                                Intereses Financieros
+                            </td>
+                            <td id='tabla' colSpan={2}>
+                                $1,024.00
+                            </td>
+
+                        </tr>
+                    </table>
                 </div>
-                <div id='separador'>
-                    <div>
-                        <h5>Utilidad</h5>
-                    </div>
-                    <div id='utilidad'>
-                        <div>
-                            $000
-                        </div>
-                    </div>
+                <div>
+                    <table id='tabla'>
+                        <tr >
+                            <td rowSpan={2} id='tabla'>
+                                Icono
+                            </td>
+                            <td id='tabla'>
+                                <h5>Gastos</h5>
+                            </td>
+
+                        </tr>
+
+                        <tr>
+                            <td id='tabla'>
+
+                            </td>
+                            <td id='tabla'>
+                                <h6>$272,621.19</h6>
+                            </td>
+
+                        </tr>
+                        <tr>
+                            <td id="tabla" >
+                                Sueldos
+                            </td>
+                            <td id='tabla' colSpan={2} >
+                                $91,137.22
+                            </td>
+
+                        </tr>
+                        <tr>
+                            <td id="tabla">
+                                Impuestos
+                            </td>
+                            <td id='tabla' colSpan={2}>
+                                $6,037.14
+                            </td>
+
+                        </tr>
+                        <tr>
+                            <td id="tabla">
+                                Operativo
+                            </td>
+                            <td id='tabla' colSpan={2}>
+                                $175,446.83
+                            </td>
+
+                        </tr>
+                    </table>
+                </div>
+                <div >
+                    <table id='tabla'>
+                        <tr >
+                            <td rowSpan={2} id='tabla' >
+                                Icono
+                            </td>
+                            <td id='tabla'>
+                                <h5>Utilidad</h5>
+                            </td>
+
+                        </tr>
+
+                        <tr>
+
+                        </tr>
+                        <tr>
+                            <td colSpan={2}>
+                                <div id='utilidad'>
+                                    
+                                        $32,617.01
+                                    
+                                </div>
+                            </td>
+                        </tr>
+
+
+                    </table>
                 </div>
             </div>
         </div>

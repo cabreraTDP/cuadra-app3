d0cc194342253a701fa6d49d3841969ddbd53fb9
GuillermoTDP <guillermomartinez@techdatapeople.com>
2022-05-24 14:15:55 -0500

    Nuevo desarrollo

--- src/Components/Modules/Nominas/NuevoNomina.js
+++ src/Components/Modules/Nominas/NuevoNomina.js
@@ -5,15 +5,35 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Post } from '../../../utils/axiosUtils';
 import { useNavigate } from 'react-router';
+import PlusButton from '../../PlusButton'
+
 
 const URL = process.env.REACT_APP_URL_URI;
 
 const NuevoNomina = () => {
+    const options=[
+        {
+            link: 'nuevo',
+            title: 'Prima Dominical'
+        },
+        {
+            link: '/app/nominas/nuevo',
+            title: 'Horas Extras'
+        },{
+            link: 'nuevo',
+            title: 'Aguinaldo'
+        },
+        {
+            link: '/app/nominas/nuevo',
+            title: 'Prima vacacional'
+        }
+    ]
     const [dataEmpleados, setDataEmpleados] = useState([{
         "Nombre": '',
         "Faltas": 0,
         "Complementos": 0,
-        "Rebajes": 0
+        "Rebajes": 0,
+        "Nuevo Concepto": ''
     }]);
 
     const [periodoInicio, setPeriodoInicio] = useState();
@@ -21,19 +41,19 @@ const NuevoNomina = () => {
     const [esquema, setEsquema] = useState();
 
     const onChangePeriodoInicio = (e) => {
-        const {value} = e.target;
+        const { value } = e.target;
 
         setPeriodoInicio(value);
     };
 
     const onChangePeriodoFin = (e) => {
-        const {value} = e.target;
+        const { value } = e.target;
 
         setPeriodoFin(value);
     };
 
     const onChangeEsquema = (e) => {
-        const {value} = e.target;
+        const { value } = e.target;
 
         setEsquema(value);
     };
@@ -43,7 +63,7 @@ const NuevoNomina = () => {
     const navigate = useNavigate();
 
     const onChangeHandler = (e) => {
-        const {name, id, value} = e.target;
+        const { name, id, value } = e.target;
 
         datos[id][name] = Number(value)
         setDatos([
@@ -51,85 +71,91 @@ const NuevoNomina = () => {
         ]);
     };
 
-    const onSubmitHandler = async(e) => {
+    const onSubmitHandler = async (e) => {
         e.preventDefault();
-        await Post('/nominas/add', {operaciones:datos, detalle:{periodoInicio,periodoFin, esquema}});
+        await Post('/nominas/add', { operaciones: datos, detalle: { periodoInicio, periodoFin, esquema } });
         navigate('/app/nominas');
     };
 
-    useEffect(()=>{
-        const getData = async(URL) => {
-            const trabajadores = await axios.get(`${URL}/trabajadores`, {withCredentials: true});
+    useEffect(() => {
+        const getData = async (URL) => {
+            const trabajadores = await axios.get(`${URL}/trabajadores`, { withCredentials: true });
             setDataEmpleados(trabajadores.data.data.map((trabajador) => (
                 trabajador.datosPersonales ? {
                     "Nombre": `${trabajador.datosPersonales.nombre} ${trabajador.datosPersonales.apellidoPaterno} ${trabajador.datosPersonales.apellidoMaterno}`,
                     "Faltas": 0,
                     "Complementos": 0,
-                    "Rebajes": 0
+                    "Rebajes": 0,
+                    "Nuevo Concepto":""
                 } :
-                {
-                    "Nombre": '',
-                    "Faltas": 0,
-                    "Complementos": 0,
-                    "Rebajes": 0
-                })
+                    {
+                        "Nombre": '',
+                        "Faltas": 0,
+                        "Complementos": 0,
+                        "Rebajes": 0,
+                        "Nuevo Concepto":""
+                    })
             ));
-            setDatos(trabajadores.data.data.map(trabajador=>(
+            setDatos(trabajadores.data.data.map(trabajador => (
                 {
-                "trabajador": trabajador._id,
-                "sueldoBase": Number(trabajador.datosLaborales.sueldo),
-                "dias": 0,
-                "complementos": 0,
-                "rebajes": 0
+                    "trabajador": trabajador._id,
+                    "sueldoBase": Number(trabajador.datosLaborales.sueldo),
+                    "dias": 0,
+                    "complementos": 0,
+                    "rebajes": 0,
+                    "Nuevo Concepto":""
                 }
             )
             ));
         }
 
         getData(URL).catch(console.error)
-        
-    },[]);
+
+    }, []);
 
     return (
         <div>
             <h1>Crear Nómina</h1>
             <div style={{ marginLeft: '50px', marginRight: '50px' }}>
-            <form onSubmit={onSubmitHandler}>
-                <div id="contenedorPrincipal">
+                <form onSubmit={onSubmitHandler}>
+                    <div id="contenedorPrincipal">
 
-                    <div id="informacion">
+                        <div id="informacion">
 
-                        <div id="informacionEspecifica">
-                            <div id="titulo">
-                                Esquema
-                            </div>
+                            <div id="informacionEspecifica">
+                                <div id="titulo">
+                                    Esquema
+                                </div>
                                 <select name="esquema" className='esquemaInput' onChange={onChangeEsquema} required>
                                     <option value="Semana">Semanal</option>
                                     <option value="Quincena">Quincenal</option>
 
                                 </select>
-                        </div>
-                        <div id="informacionEspecifica">
-                            <div id="titulo">
-                                De la fecha:
                             </div>
-                                <input className='periodoInput' type='date' name="periodoInicio" onChange={onChangePeriodoInicio} required/>
-                        </div>
-                        <div id="informacionEspecifica">
-                            <div id="titulo">
-                                A la fecha:
+                            <div id="informacionEspecifica">
+                                <div id="titulo">
+                                    De la fecha:
+                                </div>
+                                <input className='periodoInput' type='date' name="periodoInicio" onChange={onChangePeriodoInicio} required />
                             </div>
-                                <input type='date' className='periodoInput' name="periodoFin" onChange={onChangePeriodoFin} required/>
+                            <div id="informacionEspecifica">
+                                <div id="titulo">
+                                    A la fecha:
+                                </div>
+                                <input type='date' className='periodoInput' name="periodoFin" onChange={onChangePeriodoFin} required />
+                            </div>
+                            <span className="FilterEnd">
+                                <PlusButton options={options} /> 
+                            </span>
                         </div>
+
                     </div>
+                    <div >
 
-                </div>
-                <div >
+                        <TableNominasNueva titles={titles} rawData={dataEmpleados} onChangeHandler={(e) => onChangeHandler(e)} />
+                        <button type='submit' className='submitButton'>Enviar</button>
 
-                    <TableNominasNueva titles={titles} rawData={dataEmpleados} onChangeHandler={(e)=>onChangeHandler(e)}/>
-                    <button type='submit' className='submitButton'>Enviar</button>
-                    
-                </div>
+                    </div>
                 </form>
             </div>
 
